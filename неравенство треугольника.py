53e1064abce41259dd2e10ca1c5226b06dfd9f8a
tihonov.a <tihonov.a@bk.ru>
2021-09-11 17:34:45 +0300

    1

--- неравенство треугольника.py
+++ неравенство треугольника.py
@@ -2,6 +2,14 @@ a = int(input('введи а = '))
 b = int(input('введи b = '))
 c = int(input('введи c = '))
 if a + b > c and b + c > a and a + c > b:
-    print('да')
+    # print('да')
+    if a == b == c:  # a == b and b == c:
+        print('треугольник равносторонний')
+    elif a == b or b == c or c == a:
+        print('треугольник равнобедренный')
+    elif a*a + b*b == c*c or c*c + b*b == a*a or a*a + c*c == b*b:
+        print('треугольник прямоугольный')
+    else:
+        print('треугольник произвольный')
 else:
     print('нет')
