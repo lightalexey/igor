9c74b189dabb840a4fc5c91680c475cec422cd89
tihonov.a <tihonov.a@bk.ru>
2021-10-16 18:08:04 +0300

    1

--- цикл for/найти количество двухзначных чисел четных.py
+++ цикл for/найти количество двухзначных чисел четных.py
@@ -1,10 +1,8 @@
-k = 0
-k2 = 0
-k3 = 0
-k5 = 0
-summa = 0
+k = k2 = k3 = 0
+k5, summa, pr = 0, 0, 1
 for i in range(10, 99 + 1):
     summa += i
+    pr *= i
     k += 1
     if i % 2 == 0:
         k2 += 1
@@ -17,3 +15,5 @@ print(k3)
 print(k5)
 print(k)
 print(summa)
+print(summa/k)
+print(pr)
